fe111251dc857f15c780ac08f454a0878fc2754a
AlexStavrev <40688355+AlexStavrev@users.noreply.github.com>
2023-12-04 11:47:50 +0100

    Added DAL

---
+++ Security_Synopsis_Enderchest/Interfaces/IEncryptedFileRepo.cs
@@ -0,0 +1,10 @@
+﻿using Security_Synopsis_Enderchest.Models;
+
+namespace Security_Synopsis_Enderchest.Interfaces;
+public interface IEncryptedFileRepo
+{
+    Task<IEnumerable<EncryptedFile>> GetUserFilesAsync(Guid ownerGuid);
+    Task<IEnumerable<EncryptedFile>> GetSharedFilesAsync(Guid userGuid);
+    Task<Guid> CreateAsync(EncryptedFile file, User user);
+    Task<bool> ShareFileAsync(Guid userGuid, Guid fileGuid);
+}
---
+++ Security_Synopsis_Enderchest/Repository/UserRepo.cs
@@ -0,0 +1,69 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using Dapper;
+using Security_Synopsis_Enderchest.Interfaces;
+using Security_Synopsis_Enderchest.Models;
+
+namespace Security_Synopsis_Enderchest.Repository;
+
+internal class UserRepo : IUserRepo
+{
+    private readonly IDbConnection _connection;
+
+    public UserRepo(string connectionString)
+    {
+        _connection = new SqlConnection(connectionString);
+    }
+
+    public async Task<Guid?> LoginAsync(User user)
+    {
+        //Set up DynamicParameters object to pass parameters  
+        var parameters = new DynamicParameters();
+        parameters.Add("Email", user.Email);
+        parameters.Add("Password", user.Password);
+
+        //Execute stored procedure and map the returned result to a Customer object  
+        return await _connection.QuerySingleOrDefaultAsync<Guid?>("USER_MASTER_LOGIN", parameters, commandType: CommandType.StoredProcedure);
+    }
+
+    public async Task<Guid?> CreateAsync(User user)
+    {
+        Guid generatedGuid = Guid.NewGuid();
+        
+        try
+        {
+            var saving = Convert.ToBase64String(user.Password!);
+            var slat = Convert.ToBase64String(user.Password!.Take(16).ToArray());
+
+            var parameters = new DynamicParameters(); 
+            parameters.Add("Guid", generatedGuid);
+            parameters.Add("Email", user.Email);  
+            parameters.Add("Password", user.Password); 
+                
+            await _connection.QueryAsync("USER_MASTER_CREATE", parameters, commandType: CommandType.StoredProcedure);
+
+            return generatedGuid;
+        }
+        catch
+        {
+            return null;
+        }
+    }
+
+    public async Task<byte[]?> GetSaltAsync(string email)
+    {
+        //Set up DynamicParameters object to pass parameters  
+        var parameters = new DynamicParameters();
+
+        parameters.Add("Email", email);
+
+        //Execute stored procedure and map the returned result to a Customer object  
+        var returnedPasswordKey = await _connection.QuerySingleOrDefaultAsync<byte[]?>("GET_SALT", parameters, commandType: CommandType.StoredProcedure);
+        if(returnedPasswordKey != null)
+        {
+            var salt = returnedPasswordKey.Take(16).ToArray();
+            return salt;
+        }
+        return null;
+    }
+}
