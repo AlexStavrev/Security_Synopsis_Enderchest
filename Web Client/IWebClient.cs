8c06b6b95ee7abd372917fdc10a1bfca49e66cde
AlexStavrev <40688355+AlexStavrev@users.noreply.github.com>
2023-12-04 13:22:31 +0100

    Web Client

---
+++ Web Client/IWebClient.cs
@@ -0,0 +1,16 @@
+﻿using Web_Client.DTOs;
+
+namespace Web_Client;
+public interface IWebClient
+{
+    // User
+    Task<Guid?> LoginAsync(UserDto user);
+    Task<Guid?> CreateUserAsync(UserDto user);
+    Task<byte[]> GetSaltAsync(string username);
+
+    // File
+    Task<IEnumerable<EncryptedFileDto>> GetUserFilesAsync(Guid ownerGuid);
+    Task<IEnumerable<EncryptedFileDto>> GetSharedFilesAsync(Guid userGuid);
+    Task<Guid> CreateFileAsync(EncryptedFileDtoNoGuid file, Guid userGuid);
+    Task<bool> ShareFileAsync(Guid userGuid, Guid fileGuid);
+}
---
+++ Web Client/WebClientFactory.cs
@@ -0,0 +1,14 @@
+﻿using RestSharp;
+
+namespace Web_Client;
+public static class WebClientFactory
+{
+    public static T GetWebClient<T>(string connectionstring) where T : class
+    {
+        return typeof(T).Name switch
+        {
+            "IWebClient" => (new WebClient(new RestClient(connectionstring)) as T)!,
+            _ => throw new ArgumentException($"Unknown type {typeof(T).FullName}"),
+        };
+    }
+}
