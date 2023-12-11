4dec334f6908d590c737d324ab0ecff8a0ae086c
AdamGalla <ado.galla@gmail.com>
2023-12-11 12:26:09 +0100

    Create new dtos

---
+++ Web Client/DTOs/DecryptedFileDto.cs
@@ -0,0 +1,14 @@
+﻿namespace Web_Client.DTOs;
+
+public sealed record DecryptedFileDto
+{
+    public Guid Guid { get; set; }
+    public string? DecryptedFile { get; set; }
+}
+
+public sealed record DecryptedFileDtoNoGuid
+{
+    //public Guid Guid { get; set; }
+    public Guid OwnerGuid { get; set; }
+    public string? DecryptedFile { get; set; }
+}
---
+++ Web Client/DTOs/SharedFolderDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Web_Client.DTOs;
+
+public sealed record SharedFolderDto
+{
+    public Guid Guid { get; set; }
+    public Guid OwenerGuid { get; set; }
+    public byte[]? HashedShareCode { get; set; }
+    public IEnumerable<EncryptedFileDto>? EncryptedFiles { get; set; }
+    public IEnumerable<DecryptedFileDto>? DecryptedFiles { get; set; }
+}
