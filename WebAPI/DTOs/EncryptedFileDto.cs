89c8d2c07da8b1c9539909bae1c1ddcc7d3d5799
AlexStavrev <40688355+AlexStavrev@users.noreply.github.com>
2023-12-20 15:46:33 +0100

    Update EncryptedFileDto.cs

--- WebAPI/DTOs/EncryptedFileDto.cs
+++ WebAPI/DTOs/EncryptedFileDto.cs
@@ -6,10 +6,3 @@ public sealed record EncryptedFileDto
     public Guid OwnerGuid { get; set; }
     public byte[]? EncryptedFile { get; set; }
 }
-
-public sealed record EncryptedFileDtoNoGuid   
-{
-    //public Guid Guid { get; set; }
-    public Guid OwnerGuid { get; set; }
-    public byte[]? EncryptedFile { get; set; }
-}
