a37c3da5820eea52f43c027ffd7e4a1f19c2c3af
AdamGalla <ado.galla@gmail.com>
2023-12-20 11:02:39 +0100

    icons updated

--- Password Manager Desktop Client/Properties/Resources.Designer.cs
+++ Password Manager Desktop Client/Properties/Resources.Designer.cs
@@ -69,5 +69,15 @@ namespace Password_Manager_Desktop_Client.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap enderchest {
+            get {
+                object obj = ResourceManager.GetObject("enderchest", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
